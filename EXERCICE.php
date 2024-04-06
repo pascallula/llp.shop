64795ef0586746188b1388ee9db00a6e0c36a148
pascallula <166207820+pascallula@users.noreply.github.com>
2024-04-06 10:04:09 +0200

    ajout des fichiers

---
+++ EXERCICE.php
@@ -0,0 +1,19 @@
+<html>
+
+	<head>
+		<title>Notre première instruction : echo</title>
+	</head>
+	
+	<body>
+		<h2>Affichage de texte avec PHP</h2>
+			<p> Cette ligne a été écrite entièrement en (x)HTML.<br />
+	<?php echo("Celle-ci a été écrite entièrement en PHP.<br>");
+	
+		Printf("MUAMBA NZAMBI %s","Junior Jude"); 
+	?>
+	
+</p>
+
+	</body>
+
+</html>
